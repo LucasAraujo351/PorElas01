feeb540d94cfd6ecbbc378b97c54f04613396978
lucas araujo <91351613+LucasAraujo351@users.noreply.github.com>
2024-09-04 06:00:10 -0300

    Update InscricaoRepository.java
    apagando comentarios

--- backend/src/main/java/com/example/loginauthapi/repositories/InscricaoRepository.java
+++ backend/src/main/java/com/example/loginauthapi/repositories/InscricaoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface InscricaoRepository extends JpaRepository<com.example.loginauthapi.domain.user.Inscricao, String> {
-    Optional<com.example.loginauthapi.domain.user.Inscricao> findByUserIdAndCursoId(String userId, String cursoId); // Atualize para cursoId
+    Optional<com.example.loginauthapi.domain.user.Inscricao> findByUserIdAndCursoId(String userId, String cursoId); 
 }
