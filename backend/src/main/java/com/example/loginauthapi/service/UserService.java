947c173f475e9f40ee3bba4afd65b00737d04e78
lucas araujo <91351613+LucasAraujo351@users.noreply.github.com>
2024-09-04 06:01:22 -0300

    Update UserService.java
    apagando comentarios

--- backend/src/main/java/com/example/loginauthapi/service/UserService.java
+++ backend/src/main/java/com/example/loginauthapi/service/UserService.java
@@ -21,7 +21,7 @@ public class UserService {
     }
 
     public User saveUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword())); // Codifica a senha
+        user.setPassword(passwordEncoder.encode(user.getPassword())); 
         return userRepository.save(user);
     }
 
